ba662e53ef9f85af52f6abcd9c3612b0ef519be6
2laArt <goodwebalex@yandex.ru>
2022-07-30 17:52:43 +0300

    script creat DOM el

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ console.log("hi")
 
 const d = document.createElement("div")
 d.textContent = "script"
+const s = document.createElement("span")
+s.textContent = "new span"
+document.append(d)
+
+setTimeout(() => document.append(s), 1000)
